9ad2ae833083ea74fbf6ec5a593fa4e9368b9405
naverstay <naverstay.me@gmail.com>
2026-02-01 22:28:38 +0100

    vite

--- src/api.js
+++ src/api.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL + "/api", headers: {
-    Authorization: `Bearer ${import.meta.env.REACT_APP_STRAPI_TOKEN || ""}`
+    Authorization: `Bearer ${import.meta.env.VITE_STRAPI_TOKEN || ""}`
   }
 });
